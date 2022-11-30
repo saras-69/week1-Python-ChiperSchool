0d394807be46111100c71799ed196863d7af24e6
saras-69 <115482884+saras-69@users.noreply.github.com>
2022-12-01 01:29:48 +0530

    exercise1

---
+++ exercise1.py
@@ -0,0 +1,5 @@
+print("this is \\\\ double backslash \nthese are /\\/\\/\\/\\ mountains \nhe is\tawesome (use escape sequence instead of manual spaces")
+print("these are /\\/\\/\\/\\ mountains")
+print("he is\tawesome (use escape sequence instead of manual spaces")
+print("\\\" \\n \\t \\\'")
+
